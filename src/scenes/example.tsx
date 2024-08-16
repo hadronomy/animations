69134c077885a1b944cc02cd38a498bf9efec53c
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-08-16 11:56:41 +0100

    feat: add rotation animation to example scene

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,12 +1,14 @@
 import { makeScene2D } from '@motion-canvas/2d';
-import { createRef, waitFor } from '@motion-canvas/core';
+import { all, createRef, waitFor } from '@motion-canvas/core';
 
 import { Graph } from '../components/NodeGraph';
 
 export default makeScene2D(function* (view) {
   const graph = createRef<Graph>();
 
-  view.add(<Graph ref={graph} x={-500} y={-400} />);
+  view.add(<Graph ref={graph} nodeSize={120} x={-500} y={-400} />);
   yield* graph().animateIn();
+  yield* waitFor(1);
+  yield* all(graph().scale(2, 1), graph().rotation(20, 1.5));
   yield* waitFor(1);
 });
